ee8847b219761b99f2c02d831a469d287002b14f
omargad18 <omar.gad859@gmail.com>
2023-06-21 09:08:22 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print n to 98 counts in order
+ *
+ * @n: input
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+		for (i = n; i => 98; i--)
+			printf("%d, ", i);
+	else
+		for (i = n; i <= 98; i++)
+			printf("%d, ", i);
+}
