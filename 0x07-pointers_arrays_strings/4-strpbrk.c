a04738cd4e6a058bd6e8dc0468bf638f3b35b310
omargad18 <omar.gad859@gmail.com>
2023-07-04 01:28:18 +0300

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ *
+ * @s: input string
+ * @accept: input string that match
+ *
+ * Return: pointer in s that matches one of bytes in accept or NULL
+*/
+
+char *_strpbrk(char *s, char *accept);
+{
+	int i, j;
+	char *pointer;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				pointer = &s[i];
+				return (pointer);
+			}
+			j++
+		}
+		i++
+	}
+
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard
+ *
+ * @a: input number of rows
+*/
+
+void print_chessboard(char (*a)[8])
+{
+
+}
