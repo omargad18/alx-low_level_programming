b0b4debe435c6cfb0d014b34e0cb27ee23a292e5
omargad18 <omar.gad859@gmail.com>
2023-07-06 00:34:48 +0300

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * last_index - returns the last index of string (count null char)
+ * @s: ponter of string
+ * Return: int
+*/
+
+
+int is_palindrome(char *s);
+int check(char *s, int begin, int end, int mod);
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
+
+	return (n);
+}
+
+/**
+ * is_palindrome - function returns 1 if string palindrome or 0 if not
+ *
+ * @s: input string
+ *
+ * Return: 0 or 1
+*/
+
+
+int is_palindrome(char *s)
+{
+	int end = last_index(s);
+
+	return (check(s, 0, end - 1, end % 2));
+}
+
+/**
+ * check - checker for the palindrome
+ * @s: string input
+ * @begin: int from right to left
+ * @end: int from left to right
+ * @mod: input int
+ * Return 0 or 1
+*/
+
+int check(char *s, int begin, int end, int mod)
+{
+	if ((begin == end && mod != 0) || (begin == end + 1 && mod == 0))
+		return (1);
+	else if (s[begin] != s[end])
+		return (0);
+	else
+		return (check(s, begin + 1, end - 1, mod));
+}
