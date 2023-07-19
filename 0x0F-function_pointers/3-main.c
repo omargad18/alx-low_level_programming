4d11106824e36bb6167e66f3618b11e5b2bde6b1
omargad18 <omar.gad859@gmail.com>
2023-07-20 02:10:42 +0300

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code for the school students
+ * @argc: number of args
+ * @argv: arguments vector
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(98);
+
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-clac.h"
+
+/**
+ * op_add - functions adds two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function subtracts two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - function divides two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the result of division
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function modulus two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
