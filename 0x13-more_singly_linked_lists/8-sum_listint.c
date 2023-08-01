f4e072a30e1abfce7a1c323e8615a2f8a805b6ef
omargad18 <omar.gad859@gmail.com>
2023-08-01 23:37:11 +0300

    Insert

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of the data of a linked list
+ * @head: pointer to first node
+ *
+ * Return: sum
+*/
+
+int sum_listint(listint_t *head)
+{
+
+}
