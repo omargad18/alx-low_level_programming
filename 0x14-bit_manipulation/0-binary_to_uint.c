60be9ad27d1ff3e0542c40a0da6d860c8e243f37
omargad18 <omar.gad859@gmail.com>
2023-09-01 00:53:54 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b)
 	{
-		if (*b != '0' && 8b != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 		else
 			bin = bin * 2 + (*b++  - '0');
