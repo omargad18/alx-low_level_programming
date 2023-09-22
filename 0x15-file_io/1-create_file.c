f97dbebf74fd8133e862ffc9eef7d846555b3323
omargad18 <omar.gad859@gmail.com>
2023-09-23 01:22:57 +0300

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int _strlen(char *s)
  * Return: Success 1 Failure 0
 */
 
-int int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int file;
 	ssize_t bytes = 0, len = _strlen(text_content);
